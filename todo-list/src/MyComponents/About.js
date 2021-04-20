a0cf42712c0afcca9e0104e66f6ff8685357a34a
arshita129 <arshita129@gmail.com>
2021-04-20 16:04:18 +0530

    adding the files

---
+++ todo-list/src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                 Aperiam ducimus animi explicabo nihil consequatur officiis.
+                 Quod animi voluptas iste illo, quae iure ab voluptate labore distinctio. 
+                Neque sequi libero quasi!</p>
+        </div>
+    )
+}
